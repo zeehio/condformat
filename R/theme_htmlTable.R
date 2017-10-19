6c4e51c9048b13353cbc16f66f0193c5161b8f22
Sergio Oller <sergioller@gmail.com>
2017-10-19 11:30:31 +0200

    theme_htmlTable adapted to the new API

--- R/theme_htmlTable.R
+++ R/theme_htmlTable.R
@@ -4,14 +4,36 @@
 #' @seealso \code{\link[htmlTable]{htmlTable}}
 #' @examples
 #' data(iris)
-#' condformat(head(iris)) + theme_htmlTable(caption="Table 1: My iris table", rnames=FALSE)
+#' condformat(head(iris)) %>% theme_htmlTable(caption="Table 1: My iris table", rnames=FALSE)
 #' @export
 theme_htmlTable <- function(...) {
   htmlargs <- list(...)
-
+  if (length(htmlargs) > 0) {
+    if (inherits(htmlargs[[1]], "condformat_tbl")) {
+      return (theme_htmlTable_new(...))
+    }
+  }
+  warning("The condformat syntax using '+' is deprecated. See ?condformat for more information")
+  # Deprecated
   theme <- structure(list(htmlargs = htmlargs),
                      class = c("theme_htmlTable", "condformat_theme"))
   return(theme)
+}
+
+add_theme_to_condformat <- function(x, theme) {
+  condformatopts <- attr(x, "condformat")
+  condformatopts$themes <- c(condformatopts$themes, list(theme))
+  attr(x, "condformat") <- condformatopts
+  x
+}
+
+#' @rdname theme_htmlTable
+theme_htmlTable_new <- function(x, ...) {
+  htmlargs <- list(...)
+  theme <- structure(list(htmlargs = htmlargs),
+                     class = c("theme_htmlTable", "condformat_theme"))
+  x <- add_theme_to_condformat(x, theme)
+  return(x)
 }
 
 render_theme.theme_htmlTable <- function(themeobj, finaltheme, xview, ...) {
--- tests/testthat/test_theme_htmlTable.R
+++ tests/testthat/test_theme_htmlTable.R
@@ -1,6 +1,16 @@
+context("theme_htmlTable")
 test_that("theme_htmlTable works", {
   data(iris)
-  x <- condformat(head(iris)) + theme_htmlTable(caption = "Table 1: MySimpleTestCaption")
+  x <- condformat(head(iris)) %>% theme_htmlTable(caption = "Table 1: MySimpleTestCaption")
+  out <- condformat2html(x)
+  expect_match(out, "MySimpleTestCaption")
+})
+
+test_that("theme_htmlTable works (old API)", {
+  # Deprecated
+  data(iris)
+  expect_warning(t1 <- theme_htmlTable(caption = "Table 1: MySimpleTestCaption"))
+  x <- condformat(head(iris)) + t1
   out <- condformat2html(x)
   expect_match(out, "MySimpleTestCaption")
 })
