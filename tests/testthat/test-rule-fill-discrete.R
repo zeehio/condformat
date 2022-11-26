2d15bd33852828e10cb598c5aa8f8f9f47d9ad2c
Sergio Oller <sergioller@gmail.com>
2022-11-26 11:06:51 +0100

    Run vdiffr comparison

--- tests/testthat/test-rule-fill-discrete.R
+++ tests/testthat/test-rule-fill-discrete.R
@@ -89,13 +89,12 @@ test_that("rule_fill_discrete accepts a function as colours=", {
 
 
 test_that("rule_fill_discrete gtable works", {
-  # Blocked by: https://github.com/r-lib/vdiffr/pull/37
-  #skip_if_not_installed("vdiffr")
+  skip_if_not_installed("vdiffr")
   cfg <- condformat(iris[c(1,70, 120), "Species", drop = FALSE]) %>%
     rule_fill_discrete(Species) %>%
     condformat2grob(draw = FALSE)
   expect_equal(cfg$grobs[[14]]$gp$fill, "#F8766D")
   expect_equal(cfg$grobs[[15]]$gp$fill, "#00BA38")
   expect_equal(cfg$grobs[[16]]$gp$fill, "#619CFF")
-  #vdiffr::expect_doppelganger()
+  vdiffr::expect_doppelganger(title = "rule_fill_discrete gtable", cfg)
 })
