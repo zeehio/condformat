256374053ae23912815ebbebfcf5f08d08c4fe2f
Sergio Oller <sergioller@gmail.com>
2022-11-26 10:42:44 +0100

    Use vdiffr to compare generated images

--- tests/testthat/test-render-gtable.R
+++ tests/testthat/test-render-gtable.R
@@ -1,9 +1,8 @@
 test_that("basic condformat works", {
-  # Blocked by: https://github.com/r-lib/vdiffr/pull/37
-  #skip_if_not_installed("vdiffr")
+  skip_if_not_installed("vdiffr")
   cf <- condformat(iris[c(1:5,70:75, 120:125),])
   cfg <- condformat2grob(cf)
   expect_equal(nrow(cfg), 18)
   expect_equal(length(cfg), 216)
-  #vdiffr::expect_doppelganger(title = "Basic condformat image", cfg)
+  vdiffr::expect_doppelganger(title = "Basic condformat image", cfg)
 })
